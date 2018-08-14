f4ce6f4b5b59171060bf7ea036c15fa9b89c32ff
return0927 <bjleh0927@gmail.com>
2018-08-14 18:29:23 +0900

    * 2018.08.14 메인 모듈 업로드

---
+++ engine.py
@@ -0,0 +1,35 @@
+from os import urandom
+from flask import Flask, session
+from pages import blueprints as bps
+from settings import Config
+
+conf = Config()
+app_config = conf.get("Web")
+
+_temp = app_config.get("secret_key")
+if _temp:
+    _secret_key = app_config.get("secret_key")
+    del app_config["secret_key"]
+else:
+    _secret_key = urandom(32)
+
+app = Flask(__name__)
+app.secret_key = _secret_key
+
+
+@app.before_request
+def make_empty_session(*_, **__):
+    if not session.get("credentials"):
+        session['credentials'] = {
+            "logged_in": True,
+            "display_name": "placeholder"
+        }
+
+
+for module in bps:
+    if not hasattr(module, "setup"):
+        raise Exception("An improper blueprint module was passed")
+
+    module.setup(app)
+
+app.run(**app_config)
